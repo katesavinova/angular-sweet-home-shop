be49667f6e0819f265a5ce64012f162bf84f7cc1
Ekaterina <savinova.katerina@inbox.ru>
2022-01-20 14:02:31 +0400

    4.4 task

--- backend/controller/cart.js
+++ backend/controller/cart.js
@@ -1,9 +1,30 @@
+const path = require('path');
+const {readJsonFile, writeJsonFile} = require('../utils/file.utils');
+
+const FILE_PATH = path.resolve(path.dirname(require.main.filename), 'cart.json');
 module.exports.getAllProducts = async(res,req) =>{
   //ответ отдает полное содержимое корзинки
   //получив готовый список, замещаем его у себя в сервисе
+  return await readJsonFile(FILE_PATH) || [];
 }
-module.exports.addProduct = async(res,req) =>{
-  //добавление записи
 
+module.exports.addProduct = async(product) =>{
+  //добавление записи
+  const products = await this.getAllProducts();
+
+  let findIndex = products.findIndex((thing) => thing.name.toLowerCase() === product.name.toLowerCase());
+
+  if (findIndex === -1) {
+      if (product.name.trim() && product.price) {
+          products.push(product);
+          await writeJsonFile(FILE_PATH, products);
+          return products;
+      } else {
+          throw new Error("Empty fields.");
+      }
+  } else {
+      product.action = "increase";
+      return await this.changeQuantity(product);
+  }
 }
 
