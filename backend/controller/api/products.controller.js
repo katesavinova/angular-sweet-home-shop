ca17723dcecebc6c300c0915f0127638fadfd482
Ekaterina <savinova.katerina@inbox.ru>
2022-01-25 10:24:51 +0400

    fix

--- backend/controller/api/products.controller.js
+++ backend/controller/api/products.controller.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
-const usersRepository = require('../../repository/users.repository');
+const productsRepository = require('../../repository/products.repository');
 
 const router = new Router();
 
 router.get('/api/', async (req, res) => {
     const products = await productsRepository.getAll();
     res.json(products);
+});
+router.post('/api/', async (req, res) => {
+  const user = await productsRepository.add(req.body);
+  res
+      .status(201)
+      .json(user);
 });
 module.exports = router;
